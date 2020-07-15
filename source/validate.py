5dff3204bb51187ba648a5bc6af953373db00c1c
Carlos Fuentes <cfuentes@thoughtworks.com>
2020-07-15 17:48:13 -0500

    fix the score registration

--- source/validate.py
+++ source/validate.py
@@ -4,16 +4,16 @@ import numpy as np
 
 def validate(model, x_normal, x_attack, x_normal_test, track):
     print("Validating data...")
-    pred = model.predict(x_normal_test)
-    score1 = np.sqrt(metrics.mean_squared_error(pred, x_normal_test))
-    pred = model.predict(x_normal)
-    score2 = np.sqrt(metrics.mean_squared_error(pred, x_normal))
-    pred = model.predict(x_attack)
-    score3 = np.sqrt(metrics.mean_squared_error(pred, x_attack))
+    prediction = model.predict(x_normal_test)
+    score1 = np.sqrt(metrics.mean_squared_error(prediction, x_normal_test))
+    prediction = model.predict(x_normal)
+    score2 = np.sqrt(metrics.mean_squared_error(prediction, x_normal))
+    prediction = model.predict(x_attack)
+    score3 = np.sqrt(metrics.mean_squared_error(prediction, x_attack))
     print(f"Out of Sample Normal Score (RMSE): {score1}")
     print(f"Insample Normal Score (RMSE): {score2}")
     print(f"Attack Underway Score (RMSE): {score3}")
 
-    validation_metrics = {'Out of Sample Normal Score': score1, 'Insample Normal Score': score1,
-                          'Attack Underway Score': score1}
+    validation_metrics = {'Out of Sample Normal Score': score1, 'Insample Normal Score': score2,
+                          'Attack Underway Score': score3}
     track.log_metrics(validation_metrics)
