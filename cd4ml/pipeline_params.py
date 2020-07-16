3257b4d9fb3a8ac029e514b8bb9709717ffc6f66
Carlos Fuentes <cfuentes@thoughtworks.com>
2020-07-16 10:03:39 -0500

    add acceptance test logic

--- cd4ml/pipeline_params.py
+++ cd4ml/pipeline_params.py
@@ -3,7 +3,7 @@ from cd4ml.model_params import model_parameters
 
 pipeline_params = {'model_name': 'autoencoders',
                    'acceptance_metric': 'Root Mean Square Error',
-                   'acceptance_threshold_min': 0.60,
-                   'acceptance_threshold_max': 1.0,
+                   'acceptance_threshold_min': 0.0,
+                   'acceptance_threshold_max': 0.5,
                    'model_params': model_parameters
                    }
