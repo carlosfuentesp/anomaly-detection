aae5066aa87dc2d4583971e21328043c19cc378c
Carlos Fuentes <cfuentes@thoughtworks.com>
2020-07-16 15:45:00 -0500

    Modify max threshold for acceptance test

--- cd4ml/accept_model.py
+++ cd4ml/accept_model.py
@@ -7,36 +7,21 @@ TENANT = os.getenv('TENANT', 'local')
 
 def get_latest_executed_run(df_of_runs):
     filtered_dataframe = df_of_runs[df_of_runs["tags.mlflow.runName"] == os.environ["BUILD_NUMBER"]]
-    print("filtered dataframe")
-    print(filtered_dataframe.head())
     assert len(filtered_dataframe) == 1
     return filtered_dataframe
 
 
 def get_metric(metric_name, df_of_single_run):
-    print("get metric")
-    print(df_of_single_run.head())
-    print("Columns")
-    print(list(df_of_single_run.columns))
     return df_of_single_run["metrics.%s" % metric_name].head().values[0]
 
 
 def check_model_performance(metric_name, threshold_min, threshold_max):
     mlflow.set_tracking_uri(os.environ["MLFLOW_TRACKING_URL"])
     experiment = mlflow.get_experiment_by_name(TENANT)
-    experiment_id = experiment.experiment_id
-    print("experiment_id")
-    print(experiment_id)
-    runs = mlflow.search_runs(experiment_ids=experiment_id)
-    print("runs")
-    print(runs.head())
+    runs = mlflow.search_runs(experiment_ids=experiment.experiment_id)
     last_run_record = get_latest_executed_run(runs)
     metric_value = get_metric(metric_name, last_run_record)
-    print("value of the metric")
-    print(metric_value)
     run_name = last_run_record["tags.mlflow.runName"].head().values[0]
-    print("run name")
-    print(run_name)
     template = "Metric: {metric_name} for Run: {run_name} was not accepted, " \
                "value: {metric_value}, " \
                "threshold_min: {threshold_min}, threshold_max: {threshold_max}"
--- cd4ml/pipeline_params.py
+++ cd4ml/pipeline_params.py
@@ -3,7 +3,7 @@ from cd4ml.model_params import model_parameters
 
 pipeline_params = {'model_name': 'autoencoders',
                    'acceptance_metric': 'attack_underway_score',
-                   'acceptance_threshold_min': 0.0,
-                   'acceptance_threshold_max': 0.5,
+                   'acceptance_threshold_min': 0.4,
+                   'acceptance_threshold_max': 1.0,
                    'model_params': model_parameters
                    }
