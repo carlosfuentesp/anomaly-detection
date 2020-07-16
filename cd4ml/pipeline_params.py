73d6e4fb4d4f282abf6b33e0d4aef5125d5b747e
Carlos Fuentes <cfuentes@thoughtworks.com>
2020-07-16 15:40:45 -0500

    [WIP] - Add prints for debugging

--- cd4ml/pipeline_params.py
+++ cd4ml/pipeline_params.py
@@ -2,7 +2,7 @@
 from cd4ml.model_params import model_parameters
 
 pipeline_params = {'model_name': 'autoencoders',
-                   'acceptance_metric': 'Attack Underway Score',
+                   'acceptance_metric': 'attack_underway_score',
                    'acceptance_threshold_min': 0.0,
                    'acceptance_threshold_max': 0.5,
                    'model_params': model_parameters
