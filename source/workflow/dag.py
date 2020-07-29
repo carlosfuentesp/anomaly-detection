3d484ea78eda2fe41ba6ff9729865d83f35effb5
Carlos Fuentes <cfuentes@thoughtworks.com>
2020-07-28 19:52:04 -0500

    Add simple dag for testing purposes

---
+++ source/workflow/dag.py
@@ -0,0 +1,51 @@
+from datetime import timedelta
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash_operator import BashOperator
+from airflow.utils.dates import days_ago
+
+
+def pepe():
+    print("pepe")
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': days_ago(0),
+    'email': ['educharlief@gmail.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1)
+}
+
+dag = DAG(
+    'drone_data',
+    default_args=default_args,
+    description='Ingestion sensor data from google drone',
+    schedule_interval=timedelta(days=1),
+)
+
+t1 = BashOperator(
+    task_id='print_date',
+    bash_command='date',
+    dag=dag,
+)
+
+t2 = PythonOperator(
+    task_id='download_file',
+    python_callable=pepe,
+    dag=dag,
+)
+
+dag.doc_md = __doc__
+
+t1.doc_md = """\
+#### Task Documentation
+You can document your task using the attributes `doc_md` (markdown),
+`doc` (plain text), `doc_rst`, `doc_json`, `doc_yaml` which gets
+rendered in the UI's Task Instance Details page.
+![img](http://montcs.bloomu.edu/~bobmon/Semesters/2012-01/491/import%20soul.png)
+"""
+t1 >> t2
